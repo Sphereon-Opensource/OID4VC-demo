789a3138b5f5a93dce512406f37210b26cd99b18
Niels Klomp <nklomp@sphereon.com>
2023-06-06 00:32:47 +0200

    chore: Add did:web,jwk and DBC definition

--- packages/agent/src/agent.ts
+++ packages/agent/src/agent.ts
@@ -1,13 +1,13 @@
 import {createAgent, IAgent, IDIDManager, RemoveContext, TAgent} from '@veramo/core'
 import {getUniResolver} from '@sphereon/did-uni-client'
 import {
-  CredentialHandlerLDLocal,
-  LdDefaultContexts,
-  MethodNames,
-  SphereonBbsBlsSignature2020,
-  SphereonEd25519Signature2018,
-  SphereonEd25519Signature2020,
-  SphereonJsonWebSignature2020,
+    CredentialHandlerLDLocal,
+    LdDefaultContexts,
+    MethodNames,
+    SphereonBbsBlsSignature2020,
+    SphereonEd25519Signature2018,
+    SphereonEd25519Signature2020,
+    SphereonJsonWebSignature2020,
 } from '@sphereon/ssi-sdk.vc-handler-ld-local'
 import {CredentialPlugin} from '@veramo/credential-w3c'
 import {DataStore, DataStoreORM, DIDStore, KeyStore, PrivateKeyStore} from '@veramo/data-store'
@@ -15,6 +15,7 @@ import {DIDManager} from '@veramo/did-manager'
 import {EthrDIDProvider} from '@veramo/did-provider-ethr'
 import {getDidIonResolver, IonDIDProvider} from '@veramo/did-provider-ion'
 import {getDidKeyResolver, KeyDIDProvider} from '@veramo/did-provider-key'
+import {WebDIDProvider} from '@veramo/did-provider-web'
 import {DIDResolverPlugin} from '@veramo/did-resolver'
 import {KeyManager} from '@veramo/key-manager'
 import {KeyManagementSystem, SecretBox} from '@veramo/kms-local'
@@ -22,12 +23,19 @@ import {Resolver} from 'did-resolver'
 import {DB_CONNECTION_NAME, DB_ENCRYPTION_KEY, getDbConnection} from './database'
 import {ISIOPv2RP, SIOPv2RP} from '@sphereon/ssi-sdk.siopv2-oid4vp-rp-auth'
 import {IPresentationExchange, PresentationExchange} from '@sphereon/ssi-sdk.presentation-exchange'
-import {entraAndSphereonCompatibleDef, entraVerifiedIdPresentation} from './presentationDefinitions'
+import {
+    dbcConferenceAttendeeDef,
+    entraAndSphereonCompatibleDef,
+    entraVerifiedIdPresentation, fmaGuestDef
+} from './presentationDefinitions'
 import {ISIOPv2RPRestAPIOpts, SIOPv2RPRestAPI} from "@sphereon/ssi-sdk.siopv2-oid4vp-rp-rest-api";
 
+import {getResolver as getDidWebResolver} from 'web-did-resolver'
 
 import * as dotenv from "dotenv-flow";
 import {CheckLinkedDomain} from '@sphereon/ssi-sdk.siopv2-oid4vp-common'
+import {JwkDIDProvider} from "@sphereon/ssi-sdk-ext.did-provider-jwk";
+import {toJwk} from "@sphereon/ssi-sdk-ext.key-utils";
 
 dotenv.config()
 export const DIF_UNIRESOLVER_RESOLVE_URL = 'https://dev.uniresolver.io/1.0/identifiers'
@@ -44,21 +52,23 @@ export enum SupportedDidMethodEnum {
     DID_ION = 'ion',
     // DID_FACTOM = 'factom',
     DID_JWK = 'jwk',
+    DID_WEB = 'web'
 }
 
+const HOSTNAME = 'dbc2023.test.sphereon.com'
+const RP_DID_WEB = `did:web:${HOSTNAME}`
+const RP_DID_WEB_KID = `${RP_DID_WEB}#auth-key`
+
 const RP_PRIVATE_KEY_HEX = '851eb04ca3e2b2589d6f6a7287565816ee8e3126599bfeede8d3e93c53fb26e3'
-const RP_DID =
+const RP_DID_ION =
     'did:ion:EiAeobpQwEVpR-Ib9toYwbISQZZGIBck6zIUm0ZDmm9v0g:eyJkZWx0YSI6eyJwYXRjaGVzIjpbeyJhY3Rpb24iOiJyZXBsYWNlIiwiZG9jdW1lbnQiOnsicHVibGljS2V5cyI6W3siaWQiOiJhdXRoLWtleSIsInB1YmxpY0tleUp3ayI6eyJjcnYiOiJzZWNwMjU2azEiLCJrdHkiOiJFQyIsIngiOiJmUUE3WUpNRk1qNXFET0RrS25qR1ZLNW0za1VSRFc1YnJ1TWhUa1NYSGQwIiwieSI6IlI3cVBNNEsxWHlqNkprM3M2a3I2aFNrQzlDa0ExSEFpMVFTejZqSU56dFkifSwicHVycG9zZXMiOlsiYXV0aGVudGljYXRpb24iLCJhc3NlcnRpb25NZXRob2QiXSwidHlwZSI6IkVjZHNhU2VjcDI1NmsxVmVyaWZpY2F0aW9uS2V5MjAxOSJ9XX19XSwidXBkYXRlQ29tbWl0bWVudCI6IkVpQnpwN1loTjltaFVjWnNGZHhuZi1sd2tSVS1oVmJCdFpXc1ZvSkhWNmprd0EifSwic3VmZml4RGF0YSI6eyJkZWx0YUhhc2giOiJFaUJvbWxvZ0JPOERROFdpVVFsa3diYmxuMXpsRFU2Q3Jvc01wNDRySjYzWHhBIiwicmVjb3ZlcnlDb21taXRtZW50IjoiRWlEQVFYU2k3SGNqSlZCWUFLZE8yenJNNEhmeWJtQkJDV3NsNlBRUEpfamtsQSJ9fQ'
 const PRIVATE_RECOVERY_KEY_HEX = '7c90c0575643d09a370c35021c91e9d8af2c968c5f3a4bf73802693511a55b9f'
 const PRIVATE_UPDATE_KEY_HEX = '7288a92f6219c873446abd1f8d26fcbbe1caa5274b47f6f086ef3e7e75dcad8b'
-const RP_DID_KID = `${RP_DID}#auth-key`
+const RP_DID_ION_KID = `${RP_DID_ION}#auth-key`
+
+
 
 export const resolver = new Resolver({
-    /*
-    ...getUniResolver('jwk', {
-        resolveUrl: DIF_UNIRESOLVER_RESOLVE_URL
-    }),
-    */
     ...getUniResolver('ethr', {
         resolveUrl: DIF_UNIRESOLVER_RESOLVE_URL,
     }),
@@ -68,6 +78,7 @@ export const resolver = new Resolver({
         resolveUrl: DIF_UNIRESOLVER_RESOLVE_URL,
     }),
     ...getDidIonResolver(),
+    ...getDidWebResolver()
 })
 
 export const didProviders = {
@@ -81,9 +92,12 @@ export const didProviders = {
     [`${DID_PREFIX}:${SupportedDidMethodEnum.DID_ION}`]: new IonDIDProvider({
         defaultKms: KeyManagementSystemEnum.LOCAL,
     }),
-    /*[`${DID_PREFIX}:${SupportedDidMethodEnum.DID_JWK}`]: new JwkDIDProvider({
-      defaultKms: KeyManagementSystemEnum.LOCAL
-    })*/
+    [`${DID_PREFIX}:${SupportedDidMethodEnum.DID_WEB}`]: new WebDIDProvider({
+        defaultKms: KeyManagementSystemEnum.LOCAL,
+    }),
+    [`${DID_PREFIX}:${SupportedDidMethodEnum.DID_JWK}`]: new JwkDIDProvider({
+        defaultKms: KeyManagementSystemEnum.LOCAL
+    })
 }
 
 const dbConnection = getDbConnection(DB_CONNECTION_NAME)
@@ -114,12 +128,64 @@ const agent = createAgent<ISIOPv2RP & IPresentationExchange & IDIDManager>({
                 didOpts: {
                     checkLinkedDomains: CheckLinkedDomain.IF_PRESENT,
                     identifierOpts: {
-                        identifier: RP_DID,
-                        kid: RP_DID_KID,
+                        identifier: RP_DID_WEB,
+                        kid: RP_DID_WEB_KID,
                     },
                 },
             },
             instanceOpts: [
+                {
+                    definitionId: 'dbc',
+                    definition: dbcConferenceAttendeeDef,
+                    rpOpts: {
+                        didOpts: {
+                            checkLinkedDomains: CheckLinkedDomain.IF_PRESENT,
+                            identifierOpts: {
+                                identifier: RP_DID_WEB,
+                                kid: RP_DID_WEB_KID,
+                            },
+                        },
+                    }
+                },
+                {
+                    definitionId: dbcConferenceAttendeeDef.id,
+                    definition: dbcConferenceAttendeeDef,
+                    rpOpts: {
+                        didOpts: {
+                            checkLinkedDomains: CheckLinkedDomain.IF_PRESENT,
+                            identifierOpts: {
+                                identifier: RP_DID_WEB,
+                                kid: RP_DID_WEB_KID,
+                            },
+                        },
+                    }
+                },
+                {
+                    definitionId: 'fma',
+                    definition: fmaGuestDef,
+                    rpOpts: {
+                        didOpts: {
+                            checkLinkedDomains: CheckLinkedDomain.IF_PRESENT,
+                            identifierOpts: {
+                                identifier: RP_DID_WEB,
+                                kid: RP_DID_WEB_KID,
+                            },
+                        },
+                    }
+                },
+                {
+                    definitionId: fmaGuestDef.id,
+                    definition: fmaGuestDef,
+                    rpOpts: {
+                        didOpts: {
+                            checkLinkedDomains: CheckLinkedDomain.IF_PRESENT,
+                            identifierOpts: {
+                                identifier: RP_DID_WEB,
+                                kid: RP_DID_WEB_KID,
+                            },
+                        },
+                    }
+                },
                 {
                     definitionId: entraAndSphereonCompatibleDef.id,
                     definition: entraAndSphereonCompatibleDef,
@@ -156,10 +222,24 @@ const agent = createAgent<ISIOPv2RP & IPresentationExchange & IDIDManager>({
     ],
 }) as TAgent<ISIOPv2RP & IPresentationExchange & IDIDManager>
 
+agent.didManagerGet({did: RP_DID_WEB}).then(id => {
+    console.log(`==DID WEB existed:  \r\n${JSON.stringify(id, null, 2)}\r\nJWK:\r\n${JSON.stringify(toJwk(id.keys[0].publicKeyHex, 'Ed25519'), null, 2)}`)
+}).catch(error => {
+    agent.didManagerCreate({
+            provider: 'did:web',
+            alias: HOSTNAME,
+            kms: KeyManagementSystemEnum.LOCAL,
+            options: {
+                keyType: 'Ed25519'
+            }
+        }
+    ).then(id => console.log(`==DID WEB created: \r\n${JSON.stringify(id, null, 2)}\r\nJWK:\r\n${JSON.stringify(toJwk(id.keys[0].publicKeyHex, 'Ed25519'), null, 2)}`)).catch(error => console.log(`==DID WEB ERROR: ${error}`))
+})
+
 agent
     .didManagerCreate({
         provider: 'did:ion',
-        alias: RP_DID,
+        alias: RP_DID_ION,
         options: {
             kid: 'auth-key',
             anchor: false,
--- packages/agent/src/presentationDefinitions.ts
+++ packages/agent/src/presentationDefinitions.ts
@@ -1,17 +1,80 @@
-import { IPresentationDefinition } from '@sphereon/pex'
-import { Rules } from '@sphereon/pex-models'
+import {IPresentationDefinition} from '@sphereon/pex'
+import {Rules} from '@sphereon/pex-models'
+
+export const dbcConferenceAttendeeDef: IPresentationDefinition = {
+    "id": "022c2664-68cc-45cc-b291-789ce8b599eb",
+    "purpose": "We want to know your name and e-mail address (will not be stored)",
+    "input_descriptors": [
+        {
+            "id": "c2834d0e-3c95-4721-b21a-40e3d7ea2549",
+            "name": "DBC Conference 2023 Attendee",
+            "purpose": "To access this portal your DBC Conference 2023 attendance proof is required.",
+            "schema": [
+                {
+                    "uri": "DBCConferenceAttendee"
+                }
+            ],
+            "constraints": {
+                "fields": [
+                    {
+                        "path": [
+                            "$.credentialSubject.event.name",
+                            "$.vc.credentialSubject.event.name"
+                        ],
+                        "filter": {
+                            "type": "string",
+                            "pattern": "DBC Conference 2023"
+                        }
+                    }
+                ]
+            }
+        }
+    ]
+}
+
+export const fmaGuestDef: IPresentationDefinition = {
+    "id": "7601f45c-614f-4076-b13b-15c920ab1d0b",
+    "purpose": "For this portal we need your e-mail address and name from a FMA guest or DBC Conference credential",
+    "input_descriptors": [
+        {
+            "id": "4cf7cff1-0234-4f35-9d21-251668a60959",
+            "name": "FMA Guest",
+            "purpose": "You need to provide a FMA Guest Credential.",
+            "schema": [
+                {
+                    "uri": "FMAGuest"
+                }
+            ],
+            "constraints": {
+                "fields": [
+                    {
+                        "path": [
+                            "$.credentialSubject.name",
+                            "$.vc.credentialSubject.name"
+                        ],
+                        "filter": {
+                            "type": "string",
+                            "pattern": "Future Mobility Alliance Guest"
+                        }
+                    }
+                ]
+            }
+        }
+    ]
+}
+
 
 export const entraAndSphereonCompatibleDef: IPresentationDefinition = {
-  id: '9449e2db-791f-407c-b086-c21cc677d2e0',
-  purpose: 'You need to prove your Wallet Identity data',
-  submission_requirements: [
-    {
-      name: 'Sphereon Wallet Identity',
-      rule: Rules.Pick,
-      min: 0,
-      max: 1,
-      from: 'A',
-    }/* ,
+    id: '9449e2db-791f-407c-b086-c21cc677d2e0',
+    purpose: 'You need to prove your Wallet Identity data',
+    submission_requirements: [
+        {
+            name: 'Sphereon Wallet Identity',
+            rule: Rules.Pick,
+            min: 0,
+            max: 1,
+            from: 'A',
+        }/* ,
     {
       name: 'Accademic Award',
       rule: Rules.Pick,
@@ -19,64 +82,64 @@ export const entraAndSphereonCompatibleDef: IPresentationDefinition = {
       max: 1,
       from: 'B',
     },*/
-  ],
-  input_descriptors: [
-    {
-      id: 'SphereonWalletId',
-      purpose: 'Checking your Sphereon Wallet information',
-      name: 'Wallet Identity',
-      group: ['A'],
-      schema: [{ uri: 'https://sphereon-opensource.github.io/ssi-mobile-wallet/context/sphereon-wallet-identity-v1.jsonld' }],
-    },
-   /* {
-      id: 'Academic Award',
-      name: 'Academic Award',
-      group: ['B'],
-      purpose: 'To verify Academic Background',
-      schema: [
+    ],
+    input_descriptors: [
         {
-          uri: 'AcademicAward',
+            id: 'SphereonWalletId',
+            purpose: 'Checking your Sphereon Wallet information',
+            name: 'Wallet Identity',
+            group: ['A'],
+            schema: [{uri: 'https://sphereon-opensource.github.io/ssi-mobile-wallet/context/sphereon-wallet-identity-v1.jsonld'}],
         },
-      ],
-      constraints: {
-        fields: [
-          {
-            path: ['$.issuer.id', '$.issuer', '$.vc.issuer', '$.iss'],
-            filter: {
-              type: 'string',
-              pattern:
-                'did:web:launchpad.vii.electron.mattrlabs.io',
-            },
-          },
-        ],
-      },
-    },*/
-  ],
+        /* {
+           id: 'Academic Award',
+           name: 'Academic Award',
+           group: ['B'],
+           purpose: 'To verify Academic Background',
+           schema: [
+             {
+               uri: 'AcademicAward',
+             },
+           ],
+           constraints: {
+             fields: [
+               {
+                 path: ['$.issuer.id', '$.issuer', '$.vc.issuer', '$.iss'],
+                 filter: {
+                   type: 'string',
+                   pattern:
+                     'did:web:launchpad.vii.electron.mattrlabs.io',
+                 },
+               },
+             ],
+           },
+         },*/
+    ],
 }
 
 export const entraVerifiedIdPresentation: IPresentationDefinition = {
-  id: '081ea6b1-9009-4ec0-b41a-0afcf668bd50',
-  input_descriptors: [
-    {
-      id: 'TrueIdentity',
-      name: 'TrueIdentity',
-      purpose: 'To verify your demo identity',
-      schema: [
+    id: '081ea6b1-9009-4ec0-b41a-0afcf668bd50',
+    input_descriptors: [
         {
-          uri: 'TrueIdentity',
-        },
-      ],
-      constraints: {
-        fields: [
-          {
-            path: ['$.issuer', '$.vc.issuer', '$.iss'],
-            filter: {
-              type: 'string',
-              pattern: 'did.*',
+            id: 'TrueIdentity',
+            name: 'TrueIdentity',
+            purpose: 'To verify your demo identity',
+            schema: [
+                {
+                    uri: 'TrueIdentity',
+                },
+            ],
+            constraints: {
+                fields: [
+                    {
+                        path: ['$.issuer', '$.vc.issuer', '$.iss'],
+                        filter: {
+                            type: 'string',
+                            pattern: 'did.*',
+                        },
+                    },
+                ],
             },
-          },
-        ],
-      },
-    },
-  ],
+        },
+    ],
 }
