008f1b38da036e3f90f51b1bc8552cd24a4363f4
sksadjad <skhoshkhou@sphereon.com>
2023-09-29 11:01:09 +0200

    refactor:used flatmap instead of map

--- packages/oid4vci-demo-frontend/src/utils/ObjectUtils.ts
+++ packages/oid4vci-demo-frontend/src/utils/ObjectUtils.ts
@@ -1,16 +1,17 @@
 import {DataFormRow} from "../ecosystem-config";
 
 export const extractKeys = (data: DataFormRow[]): string[] => {
-    return data.flat().map(field => field.key);
+   return data.flatMap(item => item.map(field => field.key));
 };
 
 export const extractRequiredKeys = (data: DataFormRow[]): string[] => {
-  return data.flat().filter(field=> field.required).map(field => field.key);
+  return data.flatMap(item =>
+      item.filter(field => field.required)
+      .map(field => field.key)
+  );
 };
 
 export const transformDataToObject = (data: DataFormRow[]): Record<string, string> => {
-    return data.flat().reduce((acc, field) => {
-        acc[field.key] = '';
-        return acc;
-    }, {} as Record<string, string>);
+  const keyValuePairs = data.flatMap(item => item.map(field => [field.key, '']));
+  return Object.fromEntries(keyValuePairs);
 };
