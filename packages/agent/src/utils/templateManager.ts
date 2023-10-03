e9cafcd8b9ad0fe42ae443672f47b476a5d3a12b
sander <spostma@sphereon.com>
2023-10-03 09:39:15 +0200

    template update

--- packages/agent/src/utils/templateManager.ts
+++ packages/agent/src/utils/templateManager.ts
@@ -44,6 +44,9 @@ export class TemplateVCGenerator {
         this.handlebars.registerHelper('dateTimeAfterMonths', (months: number) => {
             return format(add(new Date(), {months}), this.timeFormatPattern)
         })
+        this.handlebars.registerHelper('dateTimeAfterMonths', (months: number) => {
+            return format(add(new Date(), {months}), this.timeFormatPattern)
+        })
         this.handlebars.registerHelper('toJSON', (obj) => JSON.stringify(obj));
 
         this.handlebars.registerHelper('collectionOf', function () {
