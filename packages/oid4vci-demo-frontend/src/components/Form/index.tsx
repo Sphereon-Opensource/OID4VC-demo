21230de85e9c418f758ffe2a69515422a9f1c1b0
A.G.J. Cate <brummos@gmail.com>
2023-10-14 20:27:27 +0200

    VDX-256 form component refactor

--- packages/oid4vci-demo-frontend/src/components/Form/index.tsx
+++ packages/oid4vci-demo-frontend/src/components/Form/index.tsx
@@ -41,7 +41,7 @@ const evaluateDefaultValue = (field: DataFormElement, payload: FormData): FormFi
 }
 
 const Form: FC<Props> = (props: Props): ReactElement => {
-    const { form, onChange } = props
+    const {form, onChange} = props
     const {t} = useTranslation()
     const [formData, setFormData] = useState<FormData>(getInitialState(form))
 
@@ -53,50 +53,56 @@ const Form: FC<Props> = (props: Props): ReactElement => {
         }
     }
 
-    const getFormFields = (): Array<ReactElement> => {
-        return form.map((row: DataFormRow) =>
-                <div className={style.formContentContainer}>
-                    {row.map((field: DataFormElement): ReactNode => {
-                        const defaultValue: FormFieldValue = evaluateDefaultValue(field, formData)
-                        switch (field.type) {
-                            case 'checkbox':
-                                return <SSICheckbox
-                                    borderColor={'#202776'}
-                                    selectedColor={'#202776'}
-                                    label={field.labelUrl ?
-                                        <div style={{display: 'flex', flexDirection: 'row', gap: 4}}>
-                                            {field.label &&
-                                                <div className={"poppins-normal-16"}>{t(field.label)}</div>
-                                            }
-                                            {field.labelUrl.text &&
-                                                <div className={"poppins-normal-16"} style={{color: '#4E51A7', textDecoration: 'underline'}} onClick={() => window.open('https://www.google.com', '_blank')}>{t(field.labelUrl.text)}</div>
-                                            }
-                                        </div> : field.label ? t(field.label) : undefined
-                                    }
-                                    disabled={field.readonly}
-                                    labelColor={'#424242'}
-                                    onValueChange={async (value: FormFieldValue): Promise<void> => onChangeValue(value, field.key)}
-                                />
-                            case 'text':
-                            case 'date':
-                                return <InputField
-                                    label={field.label ? t(field.label) ?? undefined : undefined}
-                                    type={field.type}
-                                    readonly={field.readonly}
-                                    defaultValue={defaultValue}
-                                    customValidation={field.customValidation ? new RegExp(field.customValidation) : undefined}
-                                    onChange={async (value: FormFieldValue): Promise<void> => onChangeValue(value, field.key)}
-                                />
-                            default:
-                                return <div/>
-                        }
-                    })}
-                </div>
-        )
+    const getFieldElementFrom = (field: DataFormElement): ReactElement => {
+        const defaultValue: FormFieldValue = evaluateDefaultValue(field, formData)
+        switch (field.type) {
+            case 'checkbox':
+                return <SSICheckbox
+                    borderColor={'#202776'}
+                    selectedColor={'#202776'}
+                    label={field.labelUrl ?
+                        <div style={{display: 'flex', flexDirection: 'row', gap: 4}}>
+                            {field.label &&
+                                <div className={"poppins-normal-16"}>{t(field.label)}</div>
+                            }
+                            {field.labelUrl.text &&
+                                <div className={"poppins-normal-16"}
+                                     style={{color: '#4E51A7', textDecoration: 'underline'}}
+                                     onClick={() => window.open('https://www.google.com', '_blank')}>{t(field.labelUrl.text)}</div>
+                            }
+                        </div> : field.label ? t(field.label) : undefined
+                    }
+                    disabled={field.readonly}
+                    labelColor={'#424242'}
+                    onValueChange={async (value: FormFieldValue): Promise<void> => onChangeValue(value, field.key)}
+                />
+            case 'text':
+            case 'date':
+                return <InputField
+                    label={field.label ? t(field.label) ?? undefined : undefined}
+                    type={field.type}
+                    readonly={field.readonly}
+                    defaultValue={defaultValue}
+                    customValidation={field.customValidation ? new RegExp(field.customValidation) : undefined}
+                    onChange={async (value: FormFieldValue): Promise<void> => onChangeValue(value, field.key)}
+                />
+            default:
+                return <div/>
+        }
+    }
+
+    const getRowElementFrom = (row: DataFormRow): ReactElement => {
+        return <div className={style.formRowContainer}>
+            {row.map((field: DataFormElement): ReactNode => getFieldElementFrom(field))}
+        </div>
+    }
+
+    const getFormFrom = (): Array<ReactElement> => {
+        return form.map((row: DataFormRow) => getRowElementFrom(row))
     }
 
     return <div className={style.container}>
-        {getFormFields()}
+        {getFormFrom()}
     </div>
 }
 
