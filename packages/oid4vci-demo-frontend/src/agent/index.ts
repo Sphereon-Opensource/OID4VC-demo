d68791c2d9159f003361784c44841021389ef830
sander <spostma@sphereon.com>
2023-10-16 13:52:34 +0200

    PR feedback

--- packages/oid4vci-demo-frontend/src/agent/index.ts
+++ packages/oid4vci-demo-frontend/src/agent/index.ts
@@ -15,21 +15,21 @@ import {getCurrentEcosystemGeneralConfig} from "../ecosystem-config"
 
 const generalConfig = getCurrentEcosystemGeneralConfig()
 
-const buildAuthentication = {
-  enabled: generalConfig.authenticationEnabled === true || generalConfig.authenticationStaticToken !== undefined,
-  staticBearerToken: generalConfig.authenticationStaticToken ?? ''
+const authentication = {
+  enabled: !!generalConfig.authenticationEnabled || !!generalConfig.authenticationStaticToken,
+  ...(generalConfig.authenticationStaticToken && { staticBearerToken: generalConfig.authenticationStaticToken })
 }
 
 const agent = createAgent<IQRCodeGenerator & ISIOPv2OID4VPRPRestClient & IOID4VCIRestClient>({
   plugins: [
     new QrCodeProvider(),
     new SIOPv2OID4VPRPRestClient({
-                baseUrl: generalConfig.oid4vpAgentBaseUrl ?? 'https://ssi.sphereon.com/agent',
-                authentication: buildAuthentication(generalConfig)
-            }),
-            new OID4VCIRestClient({
-                baseUrl: generalConfig.oid4vciAgentBaseUrl ?? 'https://ssi.sphereon.com/issuer',
-                authentication: buildAuthentication(generalConfig)
+      baseUrl: generalConfig.oid4vpAgentBaseUrl ?? 'https://ssi.sphereon.com/agent',
+      authentication: authentication
+    }),
+    new OID4VCIRestClient({
+      baseUrl: generalConfig.oid4vciAgentBaseUrl ?? 'https://ssi.sphereon.com/issuer',
+      authentication: authentication
     }),
   ]
 })
