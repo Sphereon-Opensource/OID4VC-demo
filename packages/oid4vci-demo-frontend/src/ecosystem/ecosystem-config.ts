1a9685abca97adc43ac3b3c706d5067a779afd10
sander <spostma@sphereon.com>
2023-10-20 11:23:46 +0200

    VDX-270: cleanup

--- packages/oid4vci-demo-frontend/src/ecosystem/ecosystem-config.ts
+++ packages/oid4vci-demo-frontend/src/ecosystem/ecosystem-config.ts
@@ -131,7 +131,6 @@ export interface SSICredentialIssueRequestPageConfig extends PageConfig {
     logo?: ImageProperties
     title?: string
     topParagraph?: string
-    bottomParagraphText?: string
     bottomParagraph?: string
     mobile?: {
         logo?: ImageProperties
