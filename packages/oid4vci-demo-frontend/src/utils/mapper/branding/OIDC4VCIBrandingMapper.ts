7a352d7542ff57505d85504e1e3ceb62ea217760
sksadjad <skhoshkhou@sphereon.com>
2023-10-25 09:00:02 +0200

    refactor: moved code of brandings to util file

--- packages/oid4vci-demo-frontend/src/utils/mapper/branding/OIDC4VCIBrandingMapper.ts
+++ packages/oid4vci-demo-frontend/src/utils/mapper/branding/OIDC4VCIBrandingMapper.ts
@@ -1,48 +1,71 @@
-import {CredentialsSupportedDisplay} from '@sphereon/oid4vci-common';
-import {IBasicCredentialLocaleBranding} from '@sphereon/ssi-sdk.data-store';
+import { CredentialsSupportedDisplay, CredentialSupported, EndpointMetadataResult } from '@sphereon/oid4vci-common'
+import { IBasicCredentialLocaleBranding } from '@sphereon/ssi-sdk.data-store'
 
 export const credentialLocaleBrandingFrom = async (credentialDisplay: CredentialsSupportedDisplay): Promise<IBasicCredentialLocaleBranding> => {
-  console.log(JSON.stringify(credentialDisplay, null, 2))
-  return {
-    ...(credentialDisplay.name && {
-      alias: credentialDisplay.name,
-    }),
-    ...(credentialDisplay.locale && {
-      locale: credentialDisplay.locale,
-    }),
-    ...(credentialDisplay.logo && {
-      logo: {
-        ...(credentialDisplay.logo.url && {
-          uri: credentialDisplay.logo?.url,
+    console.log(JSON.stringify(credentialDisplay, null, 2))
+    return {
+        ...(credentialDisplay.name && {
+            alias: credentialDisplay.name,
         }),
-        ...(credentialDisplay.logo.alt_text && {
-          alt: credentialDisplay.logo?.alt_text,
+        ...(credentialDisplay.locale && {
+            locale: credentialDisplay.locale,
         }),
-      },
-    }),
-    ...(credentialDisplay.description && {
-      description: credentialDisplay.description,
-    }),
+        ...(credentialDisplay.logo && {
+            logo: {
+                ...(credentialDisplay.logo.url && {
+                    uri: credentialDisplay.logo?.url,
+                }),
+                ...(credentialDisplay.logo.alt_text && {
+                    alt: credentialDisplay.logo?.alt_text,
+                }),
+            },
+        }),
+        ...(credentialDisplay.description && {
+            description: credentialDisplay.description,
+        }),
 
-    ...(credentialDisplay.text_color && {
-      text: {
-        color: credentialDisplay.text_color,
-      },
-    }),
-    ...((credentialDisplay.background_image || credentialDisplay.background_color) && {
-      background: {
-        ...(credentialDisplay.background_image && {
-          image: {
-            ...(credentialDisplay.background_image.url && {
-              uri: credentialDisplay.background_image?.url,
-            }),
-            ...(credentialDisplay.background_image.alt_text && {
-              alt: credentialDisplay.background_image?.alt_text,
-            }),
-          },
+        ...(credentialDisplay.text_color && {
+            text: {
+                color: credentialDisplay.text_color,
+            },
         }),
-        color: credentialDisplay.background_color,
-      },
-    }),
-  };
-};
+        ...((credentialDisplay.background_image || credentialDisplay.background_color) && {
+            background: {
+                ...(credentialDisplay.background_image && {
+                    image: {
+                        ...(credentialDisplay.background_image.url && {
+                            uri: credentialDisplay.background_image?.url,
+                        }),
+                        ...(credentialDisplay.background_image.alt_text && {
+                            alt: credentialDisplay.background_image?.alt_text,
+                        }),
+                    },
+                }),
+                color: credentialDisplay.background_color,
+            },
+        }),
+    };
+}
+
+export const getCredentialBrandings = async (metadata: EndpointMetadataResult): Promise<Map<string, Array<IBasicCredentialLocaleBranding>>> => {
+    const credentialBranding = new Map<string, Array<IBasicCredentialLocaleBranding>>()
+    Promise.all(
+            (metadata!.credentialIssuerMetadata!.credentials_supported as CredentialSupported[]).map(async (metadata: CredentialSupported): Promise<void> => {
+                const localeBranding: Array<IBasicCredentialLocaleBranding> = await Promise.all(
+                        (metadata.display ?? []).map(
+                                async (display: CredentialsSupportedDisplay): Promise<IBasicCredentialLocaleBranding> =>
+                                        await credentialLocaleBrandingFrom(display)
+                        ),
+                );
+
+                const credentialTypes: Array<string> =
+                        metadata.types.length > 1
+                                ? metadata.types.filter((type: string) => type !== 'VerifiableCredential')
+                                : metadata.types.length === 0
+                                        ? ['VerifiableCredential']
+                                        : metadata.types
+
+                credentialBranding.set(credentialTypes[0], localeBranding)
+            }))
+    return credentialBranding
+}
